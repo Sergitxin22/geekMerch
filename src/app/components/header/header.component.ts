78a71fd252598e9f839698e64c0f6598d2c5ce91
Sergitxin <sergiomoralescobo03@gmail.com>
2023-04-30 20:52:41 +0200

    Update: Componente Header terminado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  public raizIconos = '../../../assets/icons/';
+  public logo = '../../../assets/images/LinkedIn.svg';
 
+  public iconos = [
+    {
+      Nombre: 'Brujula',
+      Extension: '.svg',
+      Texto: 'Descubrir',
+    },
+    {
+      Nombre: 'Personas',
+      Extension: '.svg',
+      Texto: 'Personas',
+    },
+    {
+      Nombre: 'Video',
+      Extension: '.svg',
+      Texto: 'Learning',
+    },
+    {
+      Nombre: 'Maletin',
+      Extension: '.svg',
+      Texto: 'Empleos',
+    },
+  ];
+
+  public getUbicacionIcono(nombreIcono: String) {
+    let iconoBuscado!: any;
+
+    this.iconos.forEach(icono => {
+      if (icono.Nombre.toLowerCase() === nombreIcono.toLowerCase()) {
+        iconoBuscado = icono;
+      }
+    });
+
+    return iconoBuscado ? this.raizIconos + iconoBuscado.Nombre + iconoBuscado.Extension : null;
+  }
 }
